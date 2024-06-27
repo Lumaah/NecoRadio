be43ea12782bd13932545fd3ceaafe5be360eb9a
Lumaah <martinmax57@gmail.com>
2024-06-27 21:57:04 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -25,22 +25,17 @@ function fetchSongsList() {
 
 function playRandomSong() {
     if (songsList.length > 0) {
-        // Get a list of unplayed songs
         const unplayedSongs = songsList.filter(song => !playedSongs.includes(song));
         
-        // If all songs have been played, reset the playedSongs array
         if (unplayedSongs.length === 0) {
             playedSongs = [];
         }
         
-        // Choose a random song from the unplayed songs list
         const randomIndex = Math.floor(Math.random() * unplayedSongs.length);
         const selectedSong = unplayedSongs[randomIndex];
         
-        // Add the selected song to the playedSongs array
         playedSongs.push(selectedSong);
         
-        // Set and play the selected song
         audioPlayer.src = selectedSong;
         audioPlayer.play().catch(error => {
             console.log('Playback error:', error);
@@ -71,6 +66,5 @@ function setVolume(volume) {
     audioPlayer.volume = volume;
 }
 
-// Set the initial volume to 50%
 volumeSlider.value = 0.5;
 setVolume(0.5);
